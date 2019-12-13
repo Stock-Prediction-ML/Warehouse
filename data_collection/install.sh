bafd28c036fb3c2e79b4a3cf432ad758b505754e
Stuart M <stuart.j.miller.8182@gmail.com>
2019-12-13 12:17:17 -0600

    Update install.sh
    Remove R install
    Remove headless web driver install

--- data_collection/install.sh
+++ data_collection/install.sh
@@ -11,21 +11,3 @@ bash Anaconda3-2019.03-Linux-x86_64.sh
 
 # install python requirements from pip
 pip install -r requirements.txt
-
-# install R requirements
-Rscript -e ./requirements_R.R
-
-# install chrome
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get -y update
-sudo apt-get -y install google-chrome-stable
-
-# get chromedriver
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-
-# move chromedriver to install path
-mv chromedriver /usr/bin/chromedriver
-chown root:root /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
