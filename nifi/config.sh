fb25b59f23224ceb2638df01caea9a653dfcccc5
Stuart M <stuart.j.miller.8182@gmail.com>
2019-12-18 22:11:29 -0600

    Update config.sh

--- nifi/config.sh
+++ nifi/config.sh
@@ -2,9 +2,5 @@
 
 ip=${1}
 file=${2:-./nifi-1.10.0/conf/nifi.properties}
-start_ctr=${3:-./nifi-1.10.0/bin/nifi.sh}
-
 
 sed -i "/nifi.remote.input.host=/ s/nifi.remote.input.host=/nifi.remote.input.host=$1/" $2
-
-$start_ctr restart
