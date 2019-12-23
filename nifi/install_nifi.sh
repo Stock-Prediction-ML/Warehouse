6bbd46644d1f0e8e484dd45f03d8713964f67dd6
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2019-12-22 22:14:30 -0600

    Change nifi link to archive gitignore the install dir of nifi

--- nifi/install_nifi.sh
+++ nifi/install_nifi.sh
@@ -10,11 +10,10 @@ apt upgrade
 apt install openjdk-8-jdk
 
 # install nifi
-wget http://www-us.apache.org/dist/nifi/1.9.2/nifi-1.9.2-bin.tar.gz
+wget https://archive.apache.org/dist/nifi/1.9.2/nifi-1.9.2-bin.tar.gz
 tar xvf nifi-1.9.2-bin.tar.gz
 cd nifi-1.9.2
 
 # http://ijokarumawak.github.io/nifi/2017/01/27/nifi-s2s-local-to-aws/
 # make an edit on nifi.remote.input.host; add address of ec2 instance
 # then start with ./bin/nifi.sh restart
-
